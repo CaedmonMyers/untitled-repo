1bb2b36ed9d9f129f8427532a14cb7a510ba451a
CaedmonM <caedmonmyers@icloud.com>
2023-10-10 17:56:57 -0500

    Added script.js with code to animate links with the type "inlineLink". Also added code to link script.js on most HTML pages.

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.inlineLink');
+
+    links.forEach(link => {
+        const text = link.textContent;
+        link.innerHTML = '';
+
+        Array.from(text).forEach((char, i) => {
+            const span = document.createElement('span');
+            span.textContent = char;
+            span.style.color = 'white';
+            link.appendChild(span);
+        });
+
+        link.addEventListener('mouseover', () => {
+            Array.from(link.children).forEach((span, i) => {
+                setTimeout(() => {
+                    span.style.color = '#FF7438';
+                }, i * 7);
+            });
+        });
+
+        link.addEventListener('mouseout', () => {
+            Array.from(link.children).forEach((span, i) => {
+                setTimeout(() => {
+                    span.style.color = 'white';
+                }, i * 7);
+            });
+        });
+    });
+});
